25856834cb1c6dd32cd59a935f3147ff69bc450e
fafa-a <fabienpuech1@gmail.com>
2026-02-08 21:10:03 +0100

    feat: rename Open File button to Open Workspace

--- apps/ui/src/components/ui/sidebar.tsx
+++ apps/ui/src/components/ui/sidebar.tsx
@@ -55,7 +55,7 @@ export function Sidebar(props: SidebarProps) {
           >
             <path d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 2H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2z" />
           </svg>
-          Open File
+          Open Workspace
         </Button>
       </div>
 
